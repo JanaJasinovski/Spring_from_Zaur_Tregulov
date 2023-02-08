f1030070d52e9d9ce6a9ff92943e746d3e0f76e4
jana <jantschick3@gmail.com>
2023-02-08 10:28:03 +0300

    DI_with_Constructor

--- src/test/java/org/example/Test3.java
+++ src/test/java/org/example/Test3.java
@@ -7,10 +7,8 @@ public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Pet pet = new Dog();
 
-        Pet pet = context.getBean("myPet", Pet.class);
-        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
 
         context.close();
