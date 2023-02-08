df3f52213fe62bd7ec70b12955c14882ae7d0ae7
jana <jantschick3@gmail.com>
2023-02-08 17:57:44 +0300

    AfterReturning

--- src/main/java/aop/aspects/LoggingAspect.java
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -1,8 +1,12 @@
 package aop.aspects;
 
+import aop.Book;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 @Component
@@ -10,48 +14,69 @@ import org.springframework.stereotype.Component;
 @Order(1)
 public class LoggingAspect {
 
-    @Pointcut("execution(* aop.UniLibrary.*(..))")
-    private void allMethodsFromUniLibrary() {}
+//    @Pointcut("execution(* aop.UniLibrary.*(..))")
+//    private void allMethodsFromUniLibrary() {}
+//
+//    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+//    private void returnMagazineFromUniLibrary() {}
+//
+//    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+//    private void allMethodsExceptReturnMagazineFromUniLibrary() {}
+//
+//
+//    @Before("allMethodsExceptReturnMagazineFromUniLibrary()" )
+//    public void beforeAllMethodsExceptReturnMagazineFromUniLibrary() {
+//        System.out.println("beforeAllMethodsExceptReturnMagazineFromUniLibrary: writing Log #10");
+//    }
+//
+//    @Pointcut("execution(* aop.UniLibrary.get*())")
+//    private void allAddMethodsFromUniLibrary() {}
+//
+//    @Pointcut("execution(* aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromUniLibrary() {}
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary() {}
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice() {
+//        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice() {
+//        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
+//    }
 
-    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
-    private void returnMagazineFromUniLibrary() {}
+    @Before("aop.aspects.MyPointcuts.allAddMethods()")
+    public void beforeAddLoggingAdvice(JoinPoint joinPoint) {
 
-    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
-    private void allMethodsExceptReturnMagazineFromUniLibrary() {}
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("methodSignature: " + methodSignature);
+        System.out.println("methodSignature.getMethod(): " + methodSignature.getMethod());
+        System.out.println("methodSignature.getReturnType(): " + methodSignature.getReturnType());
+        System.out.println("methodSignature.getName(): " + methodSignature.getName());
 
+        if(methodSignature.getName().equals("addBook")){
+            Object[] arguments = joinPoint.getArgs();
+            for (Object obj: arguments) {
+                if(obj instanceof Book) {
+                    Book myBook = (Book) obj;
+                    System.out.println("Информация о книге: название - " + myBook.getName() + ", автор - " + myBook.getAuthor()
+                    + ", год издания - " + myBook.getYearOfPublication());
+                } else if (obj instanceof String) {
+                    System.out.println("Книгу в библиотеку добавляет - " + obj);
+                }
+            }
+        }
 
-    @Before( "allMethodsExceptReturnMagazineFromUniLibrary()" )
-    public void beforeAllMethodsExceptReturnMagazineFromUniLibrary() {
-        System.out.println("beforeAllMethodsExceptReturnMagazineFromUniLibrary: writing Log #10");
+        System.out.println("beforeAddLoggingAdvice: попытка получить книгу/журнал");
+        System.out.println("-------------------------------------");
     }
 
-    @Pointcut("execution(* aop.UniLibrary.get*())")
-    private void allGetMethodsFromUniLibrary() {}
-
-    @Pointcut("execution(* aop.UniLibrary.return*())")
-    private void allReturnMethodsFromUniLibrary() {}
-
-    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
-    private void allGetAndReturnMethodsFromUniLibrary() {}
-
-    @Before("allReturnMethodsFromUniLibrary()")
-    public void beforeReturnLoggingAdvice() {
-        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
-    }
-
-    @Before("allGetAndReturnMethodsFromUniLibrary()")
-    public void beforeGetAndReturnLoggingAdvice() {
-        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
-    }
-
-    @Before("aop.aspects.MyPointcuts.allGetMethods()")
-    public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetLoggingAdvice: попытка получить книгу/журнал");
-    }
-
-    @Before("execution(* returnBook())")
-    public void beforeReturnBookAdvice() {
-        System.out.println("beforeGetBookAdvice: попытка вернуть книгу");
-    }
+//    @Before("execution(* returnBook())")
+//    public void beforeReturnBookAdvice() {
+//        System.out.println("beforeGetBookAdvice: попытка вернуть книгу");
+//    }
 
 }
--- src/test/java/org/example/Test1.java
+++ src/test/java/org/example/Test1.java
@@ -14,13 +14,14 @@ public class Test1 {
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
 //        uniLibrary.getBook("Преступление и название");
         Book book = context.getBean("book", Book.class);
-        uniLibrary.getBook();
-        uniLibrary.returnBook();
-        uniLibrary.getMagazine();
-        uniLibrary.addBook();
+//        uniLibrary.getBook();
+//        uniLibrary.returnBook();
+//        uniLibrary.getMagazine();
+        uniLibrary.addBook("Jana", book);
+        uniLibrary.addMagazine();
 
-        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
-        schoolLibrary.getBook();
+//        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+//        schoolLibrary.getBook();
 
         context.close();
     }
