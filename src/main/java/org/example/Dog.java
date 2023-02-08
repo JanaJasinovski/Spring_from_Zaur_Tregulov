d3c90587d00e4539b803b0dccc41111348af4ebc
jana <jantschick3@gmail.com>
2023-02-08 11:38:59 +0300

    Bean_scope

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -1,9 +1,18 @@
 package org.example;
 
 public class Dog implements Pet {
+    private String name;
 
     public Dog() {
         System.out.println("Dog bean is created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
---
+++ src/test/java/org/example/Test4.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        Dog dog1 = context.getBean("myPet", Dog.class);
+        Dog dog2 = context.getBean("myPet", Dog.class);
+
+        dog1.setName("Belka");
+        dog2.setName("Strelka");
+
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (dog1 == dog2));
+        System.out.println(dog1);
+        System.out.println(dog2);
+
+        System.out.println(dog1.getName() + " " + dog2.getName());
+        context.close();
+    }
+}
