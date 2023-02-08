35e5098351d1b2086366108e9ba104e98b89d6cc
jana <jantschick3@gmail.com>
2023-02-08 15:35:35 +0300

    AOP pointcut

---
+++ src/main/java/aop/UniLibrary.java
@@ -0,0 +1,20 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniLibrary extends AbstractLibrary {
+
+//    @Override
+    public void getBook(Book book){
+        System.out.println("Мы берём книгу из UniLibrary " + book.getName());
+    }
+
+    public void returnBook() {
+        System.out.println("Мы возвращаем книгу в UniLibrary");
+    }
+
+    public void getMagazine(){
+        System.out.println("Мы берём журнал из UniLibrary");
+    }
+}
--- src/test/java/org/example/Test1.java
+++ src/test/java/org/example/Test1.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import aop.Book;
+import aop.SchoolLibrary;
+import aop.UniLibrary;
+import aop.MyConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Test1 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+
+        UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
+//        uniLibrary.getBook("Преступление и название");
+        Book book = context.getBean("book", Book.class);
+        uniLibrary.getBook(book);
+        uniLibrary.returnBook();
+        uniLibrary.getMagazine();
+
+        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+        schoolLibrary.getBook();
+
+        context.close();
+    }
 }
